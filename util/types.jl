01186cb9008cf9bf6dbd5df136dd24c3d13a38f0
Iasonas Kouveliotis-Lysikatos <iasonas@kth.se>
2020-06-02 18:29:16 +0200

    Updated data structures and added indexing constants

--- util/types.jl
+++ util/types.jl
@@ -1,19 +1,30 @@
 # constants definition for referencing PowerModels data
-const VID = 1
-const VM = 2
-const VA = 3
-const PD = 4
-const QD = 5
+# buses
+const BUS_ID = 1
+const BUS_TYPE = 2
+const VM = 3
+const VA = 4
+const PD = 5
+const QD = 6
 
+# branches
+const L_ID = 1
+const F_BUS = 2
+const T_BUS = 3
+
+# generators
+const GEN_ID = 1
+const GEN_BUS = 2
 
 
 struct PMAreas
     name::Symbol
     no_of_areas::Int64
-    clusters::Dict{String,Array{Int64,1}}
+    clusters::Dict{Int64,Array{Int64,1}}
+    data::Dict{Int64,Dict{String,Any}}
     function PMAreas(name, no_of_areas, clusters)
-    length(clusters) == no_of_areas || error("Ill-defined clustering of power model")
-    new(name, no_of_areas, clusters)
+    length(clusters) == no_of_areas || error("Ill-defined clustering of network")
+    new(name, no_of_areas, clusters, Dict())
     end
 end
 
