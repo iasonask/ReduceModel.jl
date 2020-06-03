02ed549cd641eaeb63dc71193e2980b4493770d6
Iasonas Kouveliotis-Lysikatos <iasonas@kth.se>
2020-06-03 20:21:56 +0200

    Add more matpower related constants

--- util/types.jl
+++ util/types.jl
@@ -6,15 +6,40 @@ const VM = 3
 const VA = 4
 const PD = 5
 const QD = 6
+const BUS_AREA = 7
+const GS = 8
+const BS = 9
+const BASE_KV = 10 # check numbering
+const ZONE = 11 # check numbering
+const VMAX = 12
+const VMIN = 13
+const BUS_ARRAY_SIZE = 13
 
 # branches
-const L_ID = 1
-const F_BUS = 2
-const T_BUS = 3
+# const L_ID = 1
+const F_BUS = 1
+const T_BUS = 2
+const BR_R = 3
+const BR_X = 4
+const BR_B = 5
+const BR_STATUS = 11
+const ANGMIN = 12
+const ANGMAX = 13
+const BR_ARRAY_SIZE = 13
 
 # generators
-const GEN_ID = 1
-const GEN_BUS = 2
+const GEN_BUS = 1
+const PG = 2
+const QG = 3
+const QMAX = 4
+const QMIN = 5
+const VG = 6
+const MBASE = 7
+const GEN_STATUS = 8
+const PMAX = 9
+const PMIN = 10
+
+const GEN_ARRAY_SIZE = 21
 
 
 struct PMAreas
