37720820d323b760bded065eaeacedbec2f2b0c9
Iasonas Kouveliotis-Lysikatos <iasonas@kth.se>
2020-06-04 16:58:13 +0200

    Added all required matpower constants

--- util/types.jl
+++ util/types.jl
@@ -16,12 +16,16 @@ const VMIN = 13
 const BUS_ARRAY_SIZE = 13
 
 # branches
-# const L_ID = 1
 const F_BUS = 1
 const T_BUS = 2
 const BR_R = 3
 const BR_X = 4
 const BR_B = 5
+const RATE_A = 6
+const RATE_B = 7
+const RATE_C = 8
+const TAP = 9
+const SHIFT = 10
 const BR_STATUS = 11
 const ANGMIN = 12
 const ANGMAX = 13
@@ -54,6 +58,6 @@ struct PMAreas
 end
 
 struct REIOptions
-    pf_model::DataType # ?? AbstractACPModel
+    pf_model::DataType
     pf_method::Function
 end
