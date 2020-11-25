11fa81cd3ec32f7621b5bb7aa91c51477c2870ee
Iasonas Kouveliotis-Lysikatos <iasonas@kth.se>
2020-11-25 10:58:04 +0100

    Use Requires.jl to separate the Precompilation of visualization capabilities from the core functionality

--- src/ReduceModel.jl
+++ src/ReduceModel.jl
@@ -5,18 +5,15 @@ using PowerModels
 using SparseArrays
 using LinearAlgebra
 using PyCall
-using Plots
+using Requires
 
 # Export utility
 export call_rei
 export REIOptions
-export makePlots
-export plot_grid
 
 include("util/types.jl")
 include("util/util.jl")
 include("util/partition.jl")
-include("util/reduced_plots.jl")
 
 include("rei/construct_rei.jl")
 include("rei/call_rei.jl")
@@ -27,7 +24,18 @@ const net_layout = PyNULL()
 function __init__()
     push!(pyimport("sys")["path"], joinpath(dirname(@__FILE__), "util/"))
     copy!(net_layout, pyimport("network_layout"))
+    # only load ploting functions if Plots is imported
+    @require Plots="91a5bcdd-55d7-5caf-9e0b-520d859cae80" begin
+        println("Loading Ploting functionality...")
+        using Plots
+        include("util/reduced_plots.jl")
+        export makePlots
+        export plot_grid
+    end
 end
 
+# @require Plots begin
+#   using Plots
+# end
 
 end
